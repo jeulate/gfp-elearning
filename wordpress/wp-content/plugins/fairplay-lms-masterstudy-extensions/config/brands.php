475a37131242106623ce207a3f7a569187079ff7
Juan Antonio Eulate <juan_a_eulate@hotmail.com>
2026-08-18 16:17:09 -0400

    feat: add site brand configuration foundation

---
+++ wordpress/wp-content/plugins/fairplay-lms-masterstudy-extensions/config/brands.php
@@ -0,0 +1,41 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+return [
+    'boostacademy' => [
+        'domains' => [
+            'boostacademy.com.bo',
+            'www.boostacademy.com.bo',
+        ],
+
+        'colors' => [
+            'primary'         => '#ffa800',
+            'primary_alt'     => '#f6b23a',
+            'primary_warning' => '#FF9800',
+            'primary_soft'    => '#fff8ee',
+            'primary_light'   => '#fffaf0',
+            'primary_hover'   => '#e08800',
+            'primary_dark'    => '#cc8600',
+        ],
+    ],
+
+    'matchup' => [
+        'domains' => [
+            'matchup.com.bo',
+            'www.matchup.com.bo',
+        ],
+
+        'colors' => [
+            'primary'         => '#6EC1E4',
+            'primary_alt'     => '#5E97B0',
+            'primary_warning' => '#6EC1E4',
+            'primary_soft'    => '#e4f1fa',
+            'primary_light'   => '#e4f1fa',
+            'primary_hover'   => '#3b667f',
+            'primary_dark'    => '#18648c',
+        ],
+    ],
+];
